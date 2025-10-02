fb3d035491a103796c1c0e5e5cae0bd793343fdf
Rahul CV <rahulcv447@gmail.com>
2025-10-02 12:31:55 +0530

    'updated-1'

--- src/pages/Index.jsx
+++ src/pages/Index.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
-import FloatingChat from '../Components/FloatingChat'
+import React from 'react';
+import FloatingChat from '../Components/FloatingChat';
 
-export const Index = () => {
+const Index = () => {
   return (
-    <div className="h-screen bg-chat-background">
-      <FloatingChat />
-    </div>
-  )
-}
+    <>
+      <style>{`
+        .botchat-container {
+          height: 100vh;
+          background-color: #f0f2f5; /* replace bg-chat-background color */
+        }
+      `}</style>
+
+      <div className="botchat-container">
+        <FloatingChat />
+      </div>
+    </>
+  );
+};
+
+export default Index;
